329f9a1f9118be8af1bacc10656b45f95105589c
Julian G <s87226@bht-berlin.de>
2025-04-25 13:59:04 +0200

    Refactor Thesis component to improve structure and enhance download link visibility; adjust layout for better alignment.

--- src/components/Thesis.jsx
+++ src/components/Thesis.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Element } from 'react-scroll';
+import { DiGoogleDrive } from "react-icons/di";
 
-const Thesis = ({ darkMode }) => {
+
+const Thesis = () => {
     return (
         <section className="w-full py-2 px-6 bg-[var(--background)] text-[var(--text)] font-quicksand selection:bg-[var(--accent)]">
-            <Element name="thesis" className="h-screen flex items-center justify-center">
-                <div className="max-w-4xl mx-auto text-center">
-                    Download
+            <Element name="thesis" className="h-24 flex items-center justify-center">
+                <div className="max-w-4xl mx-auto text-center flex items-center">
+                    <div className="text-xl font-medium">
+                        Hier geht's zu meiner
+                        <a href="https://drive.google.com/file/d/198y-KvVVpTpE-H1s-8tqHfH0-xBlzWzx/view"
+                            target="_blank" rel="noopener noreferrer"
+                            className="text-xl underline underline-offset-3 decoration-1 text-[var(--text)] hover:text-[var(--accent)] ml-1">
+                            Masterarbeit
+                        </a>
+                        <DiGoogleDrive className="inline w-10 h-10 fill-[var(--text)] align-[-12px] transition-colors duration-300" />
+                    </div>
                 </div>
             </Element>
         </section>
